a4ade1a165b5bfec98f98a656eb3613d421b12f6
kaelyns <kaelyn.suh@gmail.com>
2016-11-16 15:18:52 -0500

    add values for those new fields

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,22 @@ Event.create!([
   {
     name: 'Hackathon',
     location: 'Pier 27, San Francisco, CA',
-    price: 0.00
+    price: 0.00,
+    starts_at: 120.days.from_now,
+    description: 'There will be 1200 plus developers builidng new apps within 33 hours. Participants can build any app of their choice and will compete for cash, products and prizes.'
   },
   {
     name: 'Cooking Class',
     location: 'Nestle Inn, Indianapolis, IN',
-    price: 70.00
+    price: 70.00,
+    starts_at: 140.days.from_now,
+    description: 'Just in time for the holidays: learn how to make simple appetizers and small plates that will look impressive to your guests. Both vegetarian and meat eater appetizers will be covered.'
   },
   {
     name: 'Comedy for a Cause',
     location: 'Absolute Comedy, Toronto, ON',
-    price: 15.00
+    price: 15.00,
+    starts_at: 160.days.from_now,
+    description: 'A fun-filled evening of laughs, wellness and fundraising to benefit the charity Therapeutic Paws! Come mingle with some wonderful therapy dogs and cats while watching hilarious stand-up comedy. Food and drinks will be available for purchase, followed by a chance to win some great raffle prizes!'
   }
 ])
