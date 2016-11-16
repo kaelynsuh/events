31d1ee89037a14b1ee701096e4b3886fac37a64c
kaelyns <kaelyn.suh@gmail.com>
2016-11-16 15:06:16 -0500

    add more fields to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115032400) do
+ActiveRecord::Schema.define(version: 20161116200325) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
     t.string   "location"
     t.decimal  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.datetime "starts_at"
+    t.text     "description"
   end
 
 end
