d70534799c55a7fd0148064e534c5a2741a745d9
kaelyns <kaelyn.suh@gmail.com>
2016-11-16 14:43:50 -0500

    create database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Event.create!([
+  {
+    name: 'Hackathon',
+    location: 'Pier 27, San Francisco, CA',
+    price: 0.00
+  },
+  {
+    name: 'Cooking Class',
+    location: 'Nestle Inn, Indianapolis, IN',
+    price: 70.00
+  },
+  {
+    name: 'Comedy for a Cause',
+    location: 'Absolute Comedy, Toronto, ON',
+    price: 15.00
+  }
+])
