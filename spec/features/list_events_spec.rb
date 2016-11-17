4492b99b86c9d0562dca9801f45579021d7f3d2f
kaelyns <kaelyn.suh@gmail.com>
2016-11-16 20:20:21 -0500

    create attributes specs in list events spec

--- spec/features/list_events_spec.rb
+++ spec/features/list_events_spec.rb
@@ -2,8 +2,14 @@ require 'rails_helper'
 
 describe "Viewing the list of events" do
   it "shows the movies" do
+    event = Event.create(event_attributes)
+
     visit events_url
 
     expect(page).to have_text("Events")
+    expect(page).to have_text(event.name)
+    expect(page).to have_text(event.location)
+    expect(page).to have_text("$10.00")
+    expect(page).to have_text(event.description[0..10])
   end
 end
--- spec/support/attributes.rb
+++ spec/support/attributes.rb
@@ -1,9 +1,9 @@
 def event_attributes(overrides = {})
   {
     name: 'Hackathon',
-    location: 'Pier 27, San Francisco, CA',
-    price: 0.00,
+    location: 'San Francisco',
+    price: 10.00,
     starts_at: 120.days.from_now,
-    description: 'There will be 1200 plus developers builidng new apps within 33 hours. Participants can build any app of their choice and will compete for cash, products and prizes.'
+    description: 'There will be 1200 plus developers builidng new apps within 33 hours.'
   }.merge(overrides)
 end
