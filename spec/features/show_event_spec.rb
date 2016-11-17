9edb0c8816c5006645b80182a4178ef919e9ab99
kaelyns <kaelyn.suh@gmail.com>
2016-11-17 16:19:29 -0500

    add free and non free event examples in show_event_spec

--- spec/features/show_event_spec.rb
+++ spec/features/show_event_spec.rb
@@ -12,4 +12,20 @@ describe "Viewing an individual event" do
     expect(page).to have_text("$10.00")
     expect(page).to have_text(event.description)
   end
+
+  it "shows the price if the price is not $0" do
+    event = Event.create(event_attributes(price: 10.00))
+
+    visit event_url(event)
+
+    expect(page).to have_text("$10.00")
+  end
+
+  it "shows 'Free' if the price is $0" do
+    event = Event.create(event_attributes(price: 0.00))
+
+    visit event_url(event)
+
+    expect(page).to have_text("Free")
+  end
 end
