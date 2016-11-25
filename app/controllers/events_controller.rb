ccb8c0a8996708fa68aa32cddbe8d6c165386acc
kaelyns <kaelyn.suh@gmail.com>
2016-11-25 15:04:33 -0500

    create new and create actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,4 +17,15 @@ class EventsController < ApplicationController
     @event.update(event_params)
     redirect_to @event
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    event_params = params.require(:event).permit(:name, :description, :location, :price, :starts_at)
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to @event
+  end
 end
