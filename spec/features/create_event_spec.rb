58eec7658a5d759a40048bd987bd793ca84b4e3e
kaelyns <kaelyn.suh@gmail.com>
2016-11-25 14:42:54 -0500

    create spec for creating a new event

---
+++ spec/features/create_event_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "Creating a new event" do
+  it "saves the event and shows the new event's details" do
+    visit events_url
+
+    click_link 'Add New Event'
+
+    expect(current_path).to eq(new_event_path)
+
+    fill_in "Name", with: "New Event Title"
+    fill_in "Description", with: "New Event Descriptions"
+    fill_in "Location", with: "New Location"
+    fill_in "Price", with: "15.00"
+    select (Time.now.year - 1).to_s, :from => "event_starts_at_1i"
+
+    click_button 'Create Event'
+
+    expect(current_path).to eq(event_path(Event.last))
+
+    expect(page).to have_text("New Event Title")
+
+  end
+end
